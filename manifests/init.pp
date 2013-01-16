26c983e5facb88350074c5832a037e120a618ea0
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-01-16 14:49:52 +0100

    postgresql: reorganize class, use stdlib's validate_* commands

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,33 +5,46 @@ class postgresql (
   $oom_adj  = 0,
 ) inherits postgresql::params {
 
+  # Define variables
+  case $::osfamily {
+    'RedHat': {
+      $data_dir = "${base_dir}/${cluster_name}"
+      $conf_dir = $data_dir
+      $pg_hba_conf_path = "${conf_dir}/pg_hba.conf"
+      $postgresql_conf_path = "${conf_dir}/postgresql.conf"
+      $ostype = 'redhat'
+    }
+    'Debian': {
+      $data_dir = "${base_dir}/${version}/${cluster_name}"
+      $conf_dir = "/etc/postgresql/${version}/${cluster_name}"
+      $pg_hba_conf_path = "${conf_dir}/pg_hba.conf"
+      $postgresql_conf_path = "${conf_dir}/postgresql.conf"
+      $ostype = 'debian'
+    }
+    default: { fail "Unsupported OS family ${::osfamily}" }
+  }
+
+  # Checks
+  validate_string($cluster_name, '$cluster_name must be a string')
+  validate_re($base_dir, '^/', '$base_dir should be an absolute path')
+  validate_re($conf_dir, '^/', '$conf_dir should be an absolute path')
   case $::osfamily {
     'Debian' : {
       case $::lsbdistcodename {
         'lenny': {
-          if $version !~ /^(8.[34])$/ {
-            fail "version ${version} is not supported for ${::operatingsystem} ${::lsbdistcodename}!"
-          }
+          validate_re($version, '^(8\.[34])$', "version ${version} is not supported for ${::operatingsystem} ${::lsbdistcodename}!")
         }
         'squeeze': {
-          if $version !~ /^(8.4|9.0|9.1)$/ {
-            fail "version ${version} is not supported for ${::operatingsystem} ${::lsbdistcodename}!"
-          }
+          validate_re($version, '^(8\.4|9\.0|9\.1)$', "version ${version} is not supported for ${::operatingsystem} ${::lsbdistcodename}!")
         }
         'wheezy': {
-          if $version != '9.1' {
-            fail "version ${version} is not supported for ${::operatingsystem} ${::lsbdistcodename}!"
-          }
+          validate_re($version, '^9\.1$', "version ${version} is not supported for ${::operatingsystem} ${::lsbdistcodename}!")
         }
         'lucid': {
-          if $version != '8.4' {
-            fail "version ${version} is not supported for ${::operatingsystem} ${::lsbdistcodename}!"
-          }
+          validate_re($version, '^8\.4$', "version ${version} is not supported for ${::operatingsystem} ${::lsbdistcodename}!")
         }
         /^(precise|quantal)$/: {
-          if $postgresql_version != '9.1' {
-            fail "version ${version} is not supported for ${::operatingsystem} ${::lsbdistcodename}!"
-          }
+          validate_re($version, '^9\.1$', "version ${version} is not supported for ${::operatingsystem} ${::lsbdistcodename}!")
         }
         default: { fail "${::operatingsystem} ${::lsbdistcodename} is not yet supported!" }
       }
@@ -39,9 +52,7 @@ class postgresql (
     'RedHat' : {
       case $::lsbmajdistrelease {
         '6': {
-          if $version != '8.4' {
-            fail "version ${version} is not supported for ${::operatingsystem} ${::lsbdistrelease}!"
-          }
+          validate_re($version, '^8\.4$', "version ${version} is not supported for ${::operatingsystem} ${::lsbdistcodename}!")
         }
         default: { fail "${::operatingsystem} ${::lsbdistrelease} is not yet supported!" }
       }
@@ -49,25 +60,7 @@ class postgresql (
     default: { fail "${::operatingsystem} is not yet supported!" }
   }
 
-  case $::osfamily {
-    'RedHat': {
-      $data_dir = "${base_dir}/${cluster_name}"
-      $conf_dir = $data_dir
-      $pg_hba_conf_path = "${conf_dir}/pg_hba.conf"
-      $postgresql_conf_path = "${conf_dir}/postgresql.conf"
-    }
-    'Debian': {
-      $data_dir = "${base_dir}/${version}/${cluster_name}"
-      $conf_dir = "/etc/postgresql/${version}/${cluster_name}"
-      $pg_hba_conf_path = "${conf_dir}/pg_hba.conf"
-      $postgresql_conf_path = "${conf_dir}/postgresql.conf"
-    }
-    default: { fail "${::operatingsystem} is not yet supported!" }
-  }
+  # Include base
+  include "postgresql::${ostype}"
 
-  case $::osfamily {
-    'Debian' : { include postgresql::debian }
-    'RedHat' : { include postgresql::redhat }
-    default: { notice "Unsupported operatingsystem ${::operatingsystem}" }
-  }
 }
