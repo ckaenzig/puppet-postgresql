4ee61878ac5d6ce553442503036f1d487b774126
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-01-17 17:12:22 +0100

    Add doc for postgresql class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,34 @@
+# Class: postgresql
+#
+# This class provides a simple way to get a postgresql cluster up and running
+# with minimal effort.  It will install and configure all necessary packages,
+# and give you a way to set up users, databases, clients, rights, and backup.
+#
+# Parameters:
+#   ['version']       - Which version of postgresql to use. If unspecified,
+#                       the module will pick a default version based on the
+#                       operating system.
+#   ['base_dir']      - The base directory for the postgresql cluster.
+#                       Defaults are `/var/lib/pgsql` for RedHat systems
+#                       and `/var/lib/postgresql` for Debian systems.
+#   ['cluster_name']  -  The name of the cluster to create.
+#                       Defaults are `data` on RedHat systems
+#                       and `main` on Debian systems.
+#   ['oom_adj']       - Set PG_OOM_ADJ. Defaults to 0.
+#                       Only used on RedHat systems.
+#   ['check_version'] - Whether to check the version specified using the
+#                       `version` parameter. Set this to `false` if you
+#                       are using your own backported version for example.
+#
+# Actions:
+# - Creates and manages a postgresql cluster
+#
+# Requires:
+# - `puppetlabs/stdlib`
+#
+# Sample Usage:
+#   include postgresql
+#
 class postgresql (
   $version  = $postgresql::params::default_version,
   $base_dir = $postgresql::params::default_base_dir,
