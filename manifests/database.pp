21ffdb6f84993f1ef715795a167db4c98cd31fc9
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-01-17 17:32:49 +0100

    Add doc and lint postgresql::database

--- manifests/database.pp
+++ manifests/database.pp
@@ -1,25 +1,52 @@
-/*
-
-==Definition: postgresql::database
-
-Create a new PostgreSQL database
-
-*/
+# Definition: postgresql
+#
+# This definition provides a way to manage a postgresql database
+# associated to a postgresql cluster.
+#
+# You must have declared the `postgresql` class before you use
+# this definition.
+#
+# Parameters:
+#   ['ensure']      - Whether the database should be present or absent.
+#   ['owner']       - The owner of the database.
+#                     Defaults to none.
+#   ['encoding']    - The encoding used for the database.
+#                     Defaults to none.
+#   ['template']    - The template used to create the database.
+#                     Defaults to template1.
+#   ['source']      - The zipped SQL file used to initialize the database.
+#                     Defaults to none.
+#   ['overwrite']   - Whether to drop the existing database before creating it.
+#                     Defaults to false.
+#
+# Actions:
+# - Creates and manages a postgresql database.
+#
+# Requires:
+# - `puppetlabs/stdlib`
+#
+# Sample Usage:
+#   postgresql::database {"foo":
+#     ensure => present,
+#     owner  => bar,
+#   }
+#
 define postgresql::database(
   $ensure=present,
   $owner=false,
   $encoding=false,
-  $template="template1",
+  $template='template1',
   $source=false,
-  $overwrite=false) {
+  $overwrite=false,
+) {
 
   $ownerstring = $owner ? {
-    false   => "",
+    false   => '',
     default => "-O $owner"
   }
 
   $encodingstring = $encoding ? {
-    false   => "",
+    false   => '',
     default => "-E $encoding",
   }
 
@@ -27,14 +54,14 @@ define postgresql::database(
     present: {
       exec { "Create $name postgres db":
         command => "createdb $ownerstring $encodingstring $name -T $template",
-        user    => "postgres",
+        user    => 'postgres',
         unless  => "test \$(psql -tA -c \"SELECT count(*)=1 FROM pg_catalog.pg_database where datname='${name}';\") = t",
       }
     }
     absent:  {
       exec { "Remove $name postgres db":
         command => "dropdb $name",
-        user    => "postgres",
+        user    => 'postgres',
         onlyif  => "test \$(psql -tA -c \"SELECT count(*)=1 FROM pg_catalog.pg_database where datname='${name}';\") = t",
       }
     }
@@ -48,7 +75,7 @@ define postgresql::database(
     exec { "Drop database $name before import":
       command => "dropdb ${name}",
       onlyif  => "psql -l | grep '$name  *|'",
-      user    => "postgres",
+      user    => 'postgres',
       before  => Exec["Create $name postgres db"],
     }
   }
@@ -58,7 +85,7 @@ define postgresql::database(
     # TODO: handle non-gziped files
     exec { "Import dump into $name postgres db":
       command => "zcat -f ${source} | psql ${name}",
-      user    => "postgres",
+      user    => 'postgres',
       onlyif  => "test $(psql ${name} -c '\\dt' | wc -l) -eq 1",
       require => Exec["Create $name postgres db"],
     }
